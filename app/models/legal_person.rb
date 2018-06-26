7b52880d780016fafc67ac70e5b95158f5790265
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-06-26 20:28:18 -0300

    Adding CPF/CNPJ validation on models

--- app/models/legal_person.rb
+++ app/models/legal_person.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class LegalPerson < ApplicationRecord
+  # Validations 
+  validates :cnpj,
+            presence: true
 end
--- app/models/natural_person.rb
+++ app/models/natural_person.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class NaturalPerson < ApplicationRecord
+  # Validations
+  validates :cpf,
+            presence: true
 end
