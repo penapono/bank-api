05b4f0b8eae0873d2ccf3b09040f8fea6513dcf1
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-06-27 03:32:14 -0300

    Developing CreateHistory rules [RED]

--- app/business/create_history.rb
+++ app/business/create_history.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
 class CreateHistory
-  def create(origin_id: nil, destination_id:, uid:, traceable_id:, traceable_type:)
+  def create(origin_id: nil, destination_id:, uid: nil, traceable_id:, traceable_type:)
     object =
-      History.new(
-        origin_id: origin_id, destination_id: destination_id,
-        uid: uid,
-        traceable_id: traceable_id, traceable_type: traceable_type
+      build_object(
+        origin_id, destination_id,
+        uid,
+        traceable_id, traceable_type
       )
     object.save!
   rescue ActiveRecord::RecordInvalid => error
     raise StandardError, error.message
+  end
+
+  private
+
+  def build_object(origin_id, destination_id, uid, traceable_id, traceable_type)
+    History.new.tap do |object|
+      object.origin = find_by_type('Account', origin_id)
+      object.destination = find_by_type('Account', destination_id)
+      object.uid = uid
+      object.traceable = find_by_type(traceable_type, traceable_id)
+    end
+  end
+
+  def find_by_type(type, id)
+    return nil if type.blank? || id.blank?
+    Object.const_get(type.to_s).where(id: id).first
   end
 end
