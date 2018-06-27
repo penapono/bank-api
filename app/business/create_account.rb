9e05fd1e9d8173735ca0c26bd68cfdc5f5782694
Pedro Naponoceno <pedro.gnaponoceno@gmail.com>
2018-06-27 02:49:38 -0300

    Fixing broken code

--- app/business/create_account.rb
+++ app/business/create_account.rb
@@ -4,7 +4,7 @@ class CreateAccount
   def create(name:, accountable_id:, accountable_type:, account_id:, status:, balance:)
     object =
       Account.new(
-        name: name, status: status, balance: balance
+        name: name, status: status, balance: balance,
         accountable_type: accountable_type,
         accountable_id: accountable_id,
         account_id: account_id
